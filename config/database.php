9d643cb1746b83f44adfb60f8a1f64bff4bce05b
shacol44188 <smartincoleman@outlook.com>
2020-08-07 15:49:36 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
       'username' => env('DB_USERNAME'),
       'password' => env('DB_PASSWORD'),
       'options'  => [
-          'database' => 'admin' // sets the authentication database required by mongo 3
+          'database' => 'admin' 
       ]
     ],
     'tyret' => [
